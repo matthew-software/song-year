8c187242eb5598f279a2b9cf0704660bd66dab94
matthew-software <mglaine@yahoo.com>
2024-03-11 20:15:05 -0700

    Fixed database path again.

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ DATABASE = 'final-project.db'
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
+        db = g._database = sqlite3.connect(path.join(ROOT, DATABASE))
     db.row_factory = sqlite3.Row
     return db
 
